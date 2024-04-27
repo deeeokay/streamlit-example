42b6baf514167c9b7d5c5811aeb3b9f24ad32986
deeeokay <165281611+deeeokay@users.noreply.github.com>
2024-04-28 01:23:15 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,29 @@
-import altair as alt
-import numpy as np
-import pandas as pd
 import streamlit as st
 
-"""
-# Welcome to Ds Space
+# Streamlit app
+st.subheader('D Central Perk')
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+# Get OpenAI API key, SERP API key and search query
+with st.sidebar:
+    openai_api_key = st.text_input("OpenAI API key", value="", type="password")
+    st.caption("*If you don't have an OpenAI API key, get it [here](https://platform.openai.com/account/api-keys).*")
+    serpapi_api_key = st.text_input("SERP API Key", type="password")
+    st.caption("*If you don't have a SERP API key, get it [here](https://serpapi.com).*")
+search_query = st.text_input("Search Query")
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
-
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
-
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
-
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
-
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+# If the 'Search' button is clicked
+if st.button("Search"):
+    # Validate inputs
+    if not openai_api_key.strip() or not serpapi_api_key.strip() or not search_query.strip():
+        st.error(f"Please provide the missing fields.")
+    else:
+        try:
+            with st.spinner('Please wait...'):
+              # Initialize the OpenAI module, load the SerpApi tool, and run the search query using an agent
+              llm=OpenAI(temperature=0, openai_api_key=openai_api_key, verbose=True)
+              tools = load_tools(["serpapi"], llm, serpapi_api_key=serpapi_api_key)
+              agent = initialize_agent(tools, llm, agent="zero-shot-react-description", verbose=True)
+              result = agent.run(search_query)
+              st.success(result)
+        except Exception as e:
+            st.exception(f"An error occurred: {e}")
